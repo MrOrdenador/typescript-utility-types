c133ee4f5b42d189adf9cc17155788a0d918b65e
MrOrdenador <srzaixgd@gmail.com>
2025-12-31 15:21:05 +0100

    Uploading the second lesson: Required<T>

---
+++ 02-required.ts
@@ -0,0 +1,20 @@
+// --- 02: Required<T> ---
+// Hace que todas las propiedades de un tipo (incluso las opcionales) sean requeridas.
+
+interface ProductIncomplete {
+  id: number;
+  name?: string;
+  price?: number;
+}
+
+type ProductComplete = Required<ProductIncomplete>;
+
+const product: ProductIncomplete = {
+  id: 1,
+};
+
+const productComplete: ProductComplete = {
+  id: 1,
+  name: "Switch 2",
+  price: 299,
+};
