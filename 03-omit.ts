433e1d9cd8840268ccfb68087d608865d7805583
MrOrdenador <srzaixgd@gmail.com>
2025-12-31 15:21:29 +0100

    Uploading the third lesson: Omit<T, whatyouwantoomit>

---
+++ 03-omit.ts
@@ -0,0 +1,24 @@
+// --- 03: Omit<T, K> ---
+// Crea un tipo eliminando las propiedades K del tipo T.
+
+interface Avenger {
+  name: string;
+  power: number;
+  weapon?: string;
+  alive: boolean;
+}
+
+type AvengerWithoutSpoilers = Omit<Avenger, "alive">;
+
+const ironMan: AvengerWithoutSpoilers = {
+  name: "Iron Man",
+  power: 9000,
+  weapon: "Un millonario con armadura",
+};
+
+type AvengerBasicInfo = Omit<Avenger, "weapon" | "alive">;
+
+const ironManBasic: AvengerBasicInfo = {
+  name: "Iron Man",
+  power: 9000,
+};
